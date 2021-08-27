75a52ee919869e90c97d33c39650a6883fbef3e4
RaíssaAreal <38378004+Teddy1LeBunny@users.noreply.github.com>
2021-08-27 09:28:35 -0300

    Add files via upload

--- projeto_final_Gama/js/script.js
+++ projeto_final_Gama/js/script.js
@@ -59,3 +59,4 @@ function validacao(){
         document.getElementById('error').style.display = 'block';
     }
 }
+
---
+++ projeto_final_Gama/js/telefone.js
@@ -0,0 +1,44 @@
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
+}
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+    v=v.replace(/\D/g,""); 
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); 
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2"); 
+    return v;
+}
+function id( telefone1 ){
+	return document.getElementById( telefone1 );
+}
+window.onload = function(){
+	id('telefone1').onkeyup = function(){
+		mascara( this, mtel );
+	}
+}
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
+}
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+    v=v.replace(/\D/g,""); 
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); 
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2"); 
+    return v;
+}
+function id( el ){
+	return document.getElementById( el );
+}
+window.onload = function(){
+	id('telefone2').onkeyup = function(){
+		mascara( this, mtel );
+	}
+}
